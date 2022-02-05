c217cba354c3dd3e0ae54640f121f8f0cdfcb7b9
Eunho Moon <eunho.moon@gmail.com>
2022-02-05 18:49:57 +0900

    Naver Open Api

--- src/main/java/com/janek/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/janek/security1/config/oauth/PrincipalOauth2UserService.java
@@ -1,5 +1,7 @@
 package com.janek.security1.config.oauth;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 import com.janek.security1.config.auth.PrincipalDetails;
 import com.janek.security1.config.oauth.provider.FacebookUserInfo;
 import com.janek.security1.config.oauth.provider.GoogleUserInfo;
+import com.janek.security1.config.oauth.provider.NaverUserInfo;
 import com.janek.security1.config.oauth.provider.OAuth2UserInfo;
 import com.janek.security1.model.User;
 import com.janek.security1.repository.UserRepository;
@@ -38,8 +41,11 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 		} else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
 			System.out.println("페이스북 로그인 요청");
 			oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+		} else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+			System.out.println("네이버 로그인 요청");
+			oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
 		} else {
-			System.out.println("구글이나 페이스북만 지원합니다.");
+			System.out.println("구글이나 페이스북, 네이버만 지원합니다.");
 		}
 		
 		String provider = oAuth2UserInfo.getProvider();	// google, facebook
@@ -52,6 +58,7 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 		User userEntity = userRepository.findByUsername(username);
 		
 		if (userEntity == null) {
+			System.out.println("OAuth 로그인이 최초입니다.");
 			userEntity = User.builder()
 					.username(username)
 					.password(password)
@@ -62,6 +69,8 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 					.build();
 			
 			userRepository.save(userEntity);
+		} else {
+			System.out.println("로그인을 이미 한적이 있습니다. 자동 회원가입이 되어있습니다.");
 		}
 		
 		return new PrincipalDetails(userEntity, oAuth2User.getAttributes());
